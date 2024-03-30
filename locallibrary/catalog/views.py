d4aed5a1e34e1b8034fa50a48c2efc9268196fed
vinayak <trivedivinayak57@gmail.com>
2024-03-30 09:37:55 +0530

    added book and author page in local library website

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -24,3 +24,17 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
